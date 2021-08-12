1a13ff983d08157b1cd16d5662e0fcde73281c8f
Евсюков Николай Михайлович <nikolasgrizli@gmail.com>
2021-08-12 23:22:39 +0300

    change form component

--- static/src/js/components/formComponent.js
+++ static/src/js/components/formComponent.js
@@ -1,229 +1,156 @@
-var testForm = document.getElementById("form1");
-if (testForm) {
-    window.formComponent = {
-        form: document.getElementById("form1"),
-        validationFields: () => {
-            // console.log(document.querySelectorAll("#form1 [data-validate]"));
-            return document.querySelectorAll("#form1 [data-validate]");
-        },
-        nextStepBtns: document
-            .getElementById("form1")
-            .querySelectorAll(".formNextStep"),
+const axios = require("axios");
+const testForms = document.querySelectorAll(".form_js-validate");
 
-        // if(validationFields.length < 1) return;
+window.formComponent = {
+    init: () => {
+        testForms.forEach((form) => {
+            const fields = form.querySelectorAll("[data-validate]");
+            formComponent.validationOnSubmit(fields, form);
+            formComponent.validateOnEntry(fields, form);
+        });
+    },
 
-        init: () => {
-            // console.log(validationFields.length);
-            // form.classList.add('_validation-start');
+    modelValidation: (validationFields, form) => {
+        validationFields.forEach((field) => {
+            formComponent.validateFields(field);
+        });
+        let checks = form.querySelector(".error:not(.hidden)");
+        if (form.contains(checks)) {
+            // here you can add some error msg or else ...
+        } else {
+            formSending();
+        }
+    },
 
-            formComponent.validationOnSubmit();
-            formComponent.validationOnNextStep();
-            formComponent.validateOnEntry();
-        },
+    validationOnSubmit: (validationFields, form) => {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
 
-        validationOnNextStep: () => {
-            formComponent.nextStepBtns.forEach((btn) => {
-                btn.addEventListener("click", (e) => {
-                    e.preventDefault();
-                    formComponent.modelValidation();
-                });
-            });
-        },
+            formComponent.modelValidation(validationFields, form);
+        });
+    },
 
-        modelValidation: (goToNextPage = true) => {
-            formComponent.validationFields().forEach((field) => {
+    validateOnEntry: (validationFields, form) => {
+        validationFields.forEach((field) => {
+            field.addEventListener("input", (event) => {
                 formComponent.validateFields(field);
             });
-            const checks = formComponent.form.querySelectorAll(
-                ".error:not(.hidden):not(.date-error)"
-            );
-            if (
-                window.userType !== "client" &&
-                goToNextPage &&
-                !checks.length
-            ) {
-                nextPage();
+        });
+    },
+
+    validateFields: (input) => {
+        const data = input.dataset.validate;
+
+        if (~data.indexOf("no-empty")) {
+            if (input.value.trim() === "") {
+                formComponent.setStatus(input, "error");
+            } else {
+                formComponent.setStatus(input, "success");
             }
-        },
+        }
+        if (~data.indexOf("number")) {
+            const numReg = /^\d+$/;
+            if (numReg.test(input.value) && input.value.trim() !== "") {
+                formComponent.setStatus(input, "success");
+            } else {
+                formComponent.setStatus(input, "error");
+            }
+        }
+        if (~data.indexOf("email")) {
+            const emailReg = /\S+@\S+\.\S+/;
+            if (emailReg.test(input.value) && input.value.trim() !== "") {
+                formComponent.setStatus(input, "success");
+            } else {
+                formComponent.setStatus(input, "error");
+            }
+        }
 
-        validationOnSubmit: () => {
-            formComponent.form.addEventListener("submit", (e) => {
-                formComponent.modelValidation();
-                const date = document.querySelector("#date");
-                if (date && !date.value) {
-                    const dateErrElement = document.querySelector(
-                        ".date-error"
-                    );
-                    if (dateErrElement) {
-                        dateErrElement.classList.remove("hidden");
-                    }
-                }
+        if (~data.indexOf("select")) {
+            if (input.value === "") {
+                formComponent.setStatus(input, "error");
+            } else {
+                formComponent.setStatus(input, "success");
+            }
+        }
+        if (~data.indexOf("checkbox")) {
+            // console.log(input.checked);
+            if (input.checked) {
+                formComponent.setStatus(input, "success");
+            } else {
+                formComponent.setStatus(input, "error");
+            }
+        }
 
-                const checks = formComponent.form.querySelectorAll(
-                    ".error:not(.hidden)"
-                );
+        if (~data.indexOf("phone")) {
+            const phoneReg =
+                /^(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}$/gm;
+            if (phoneReg.test(input.value) && input.value.trim() !== "") {
+                formComponent.setStatus(input, "success");
+            } else {
+                formComponent.setStatus(input, "error");
+            }
+        }
+    },
 
-                if (!!checks.length) {
-                    e.preventDefault();
-                }
+    // set/remove class final fx
+    setStatus: (field, status) => {
+        const errorMessage = field.parentElement.querySelector(".error");
+
+        // console.log(errorMessage);
+        if (status === "success") {
+            field.classList.remove("input-error");
+            errorMessage.classList.add("hidden");
+        }
+        if (status === "error") {
+            field.classList.add("input-error");
+            errorMessage.classList.remove("hidden");
+        }
+    },
+};
+
+formComponent.init();
+
+
+
+const tForm = document.querySelector('.some-form');
+
+if(!!tForm){
+    async function formSending() {
+        let newFormData = new FormData(),
+            action = tForm.action;
+
+        // google captcha
+        // winnerFormData.append(
+        //     "g-recaptcha-response",
+        //     document.getElementById("g-recaptcha-response-60edd5e3419e6").value
+        // );
+
+
+        newFormData.append("data", "some value");
+        try {
+            const response = await axios({
+                method: "post",
+                url: action,
+                data: newFormData,
+                headers: { "Content-Type": "multipart/form-data" },
             });
-        },
 
-        validateOnEntry: () => {
-            formComponent.validationFields().forEach((field) => {
-                field.addEventListener("input", (event) => {
-                    formComponent.validateFields(field);
-                });
-            });
-        },
-
-        validateFields: (input) => {
-            const data = input.dataset.validate;
-
-            if (~data.indexOf("no-empty")) {
-                if (input.value.trim() === "") {
-                    formComponent.setStatus(input, "error");
-                } else {
-                    formComponent.setStatus(input, "success");
-                }
+            if (response.data.errors) {
+                // fn(response.data.errors);
             }
+            // if all ok
 
-            if (~data.indexOf("email")) {
-                const emailReg = /\S+@\S+\.\S+/;
-                if (emailReg.test(input.value) && input.value.trim() !== "") {
-                    formComponent.setStatus(input, "success");
-                } else {
-                    formComponent.setStatus(input, "error");
-                }
-            }
+        } catch (err) {
+        } finally {
+             // google captcha
+            // const token = await grecaptcha.execute(
+            //     recaptchaToken,
+            //     { action: "home_form" }
+            // );
 
-            if (~data.indexOf("select")) {
-                if (input.value === "") {
-                    formComponent.setStatus(input, "error");
-                } else {
-                    formComponent.setStatus(input, "success");
-                }
-            }
-            if (~data.indexOf("checkbox")) {
-                // console.log(input.checked);
-                if (input.checked) {
-                    formComponent.setStatus(input, "success");
-                } else {
-                    formComponent.setStatus(input, "error");
-                }
-            }
-
-            if (~data.indexOf("phone")) {
-                const phoneReg = /^(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}$/gm;
-                if (phoneReg.test(input.value) && input.value.trim() !== "") {
-                    formComponent.setStatus(input, "success");
-                } else {
-                    formComponent.setStatus(input, "error");
-                }
-            }
-        },
-
-        // set/remove class final fx
-        setStatus: (field, status) => {
-            const errorMessage =
-                field.parentNode.querySelector(".error") ||
-                field.closest(".form__field").querySelector(".error");
-
-            // console.log(errorMessage);
-            if (status === "success") {
-                field.classList.remove("input-error");
-                errorMessage.classList.add("hidden");
-            }
-            if (status === "error") {
-                field.classList.add("input-error");
-                errorMessage.classList.remove("hidden");
-            }
-        },
-    };
-
-    formComponent.init();
+            // document.getElementById(
+            //     "g-recaptcha-response-60edd5e3419e6"
+            // ).value = token;
+        }
+    }
 }
-
-//form data when select store
-// (function formData() {
-//     document.addEventListener("DOMContentLoaded", function () {
-//         let select = document.getElementById("store_id");
-//         if (typeof storesLine !== "undefined" && select) {
-//             select.addEventListener("change", function () {
-//                 document
-//                     .getElementById("js-business-form-inner")
-//                     .classList.remove("invisible");
-
-//                 /**
-//                  * @type {HTMLInputElement[]}
-//                  */
-//                 const inputsToReset = document.querySelectorAll(
-//                     ".reset-on-change-store"
-//                 );
-//                 inputsToReset.forEach((inputToReset) => {
-//                     inputToReset.value = null;
-//                     inputToReset.parentNode.classList.remove(
-//                         "form__field_active"
-//                     );
-//                 });
-
-//                 if (select.value !== newStoreId) {
-//                     storesLine.forEach((store) => {
-//                         if (store.id === select.value) {
-//                             for (fieldName in store) {
-//                                 let elem = document.getElementById(fieldName);
-//                                 if (elem && store.hasOwnProperty(fieldName)) {
-//                                     elem.value = store[fieldName];
-//                                     elem.parentNode.classList.add(
-//                                         "form__field_active"
-//                                     );
-//                                 }
-//                             }
-//                         }
-//                     });
-//                     formComponent.modelValidation(false);
-//                     document.getElementById("store_name").dataset.validate = "";
-//                     document
-//                         .getElementById("store_name")
-//                         .classList.remove("input-error");
-//                     document
-//                         .querySelector("#store_name ~ .error")
-//                         .classList.add("hidden");
-//                     document
-//                         .getElementById("store-text-wrapper")
-//                         .classList.add("hidden");
-//                 } else {
-//                     // select.closest('.form__line').classList.add('hidden');
-//                     document.getElementById("store_name").dataset.validate =
-//                         "no-empty";
-//                     document
-//                         .getElementById("store-text-wrapper")
-//                         .classList.remove("hidden");
-//                 }
-//             });
-//         }
-//     });
-// })();
-
-(function focusInput() {
-    function focus() {
-        if (this.value == "") {
-            this.parentNode.classList.add("form__field_active");
-        }
-    }
-
-    function blur() {
-        if (this.value === "") {
-            this.parentNode.classList.remove("form__field_active");
-        }
-    }
-
-    var inputs = document.getElementsByClassName("form__input");
-
-    for (var i = 0; i < inputs.length; i++) {
-        var input = inputs[i];
-
-        input.addEventListener("focus", focus);
-        input.addEventListener("blur", blur);
-    }
-})();
